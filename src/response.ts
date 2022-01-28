24782347e828569c0e4290ce068b47fa4882f9d3
Fabian Cook <hello@fabiancook.dev>
2022-01-29 12:24:05 +1300

    Response.json as per https://github.com/whatwg/fetch/issues/1389

--- src/response.ts
+++ src/response.ts
@@ -78,6 +78,17 @@ class Response extends Body {
     return response;
   }
 
+  static json(body: unknown, init?: ResponseInit) {
+    const headers = new Headers(init?.headers);
+    if (!headers.has("Content-Type")) {
+      headers.set("Content-Type", "application/json");
+    }
+    return new Response(JSON.stringify(body), {
+      ...init,
+      headers
+    });
+  }
+
 }
 
 export { Response };
